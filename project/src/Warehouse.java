803deb3c0f7c6eeb56a70dc39db0a1249bfbfcc6
Jonathan Ruan <Jonathan Ruan>
2017-03-10 16:42:07 -0500

    Changed Warehouse.java method numberFascia to take a string value for the zone as opposed to int representation.

--- project/src/Warehouse.java
+++ project/src/Warehouse.java
@@ -29,22 +29,17 @@ public class Warehouse {
     /**
      * @param location A specific location in Warehouse represented as a list of ints
      *                 containing 4 pieces of information: the
-     *                 zone (an integer in the range [0..1] where 0 is zone A and 1 is zone B),
+     *                 zone (a string in the range [A..B]),
      *                 the aisle number (an integer
      *                 in the range [0..1]), the rack number (an integer in the range ([0..2]),
      *                 and the level on the rack (an integer in the range [0..3]).
      * @return The number of fascia at location.
      */
-    public int numberFascia(int[] location) {
-        String zone = "";
-        if (location[0] == 0) {
-            zone = "A";
-        } else if (location[0] == 1) {
-            zone = "B";
-        }
-        int aisle = location[1];
-        int rack = location[2];
-        int level = location[3];
+    public int numberFascia(Object[] location) {
+        String zone = (String) location[0];
+        int aisle = (int) location[1];
+        int rack = (int) location[2];
+        int level = (int) location[3];
         return this.floor.get(zone)[aisle][rack][level];
     }
 
